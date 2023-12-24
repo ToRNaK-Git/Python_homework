7e84401c9742f734800121975fd8997ff771e36b
Admin <paleclandusha@gmail.com>
2023-12-24 12:47:12 +0200

    DONE

--- ex32.py
+++ ex32.py
@@ -4,22 +4,26 @@ class Fraction:
         self.b = b
 
     def __mul__(self, other):
-        pass
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.a, self.b * other.b)
 
     def __add__(self, other):
-        pass
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.b + self.b * other.a, self.b * other.b)
 
     def __sub__(self, other):
-        pass
+        if isinstance(other, Fraction):
+            return Fraction(self.a * other.b - self.b * other.a, self.b * other.b)
 
     def __eq__(self, other):
-        pass
+        if isinstance(other, Fraction):
+            return self.a * other.b == self.b * other.a
 
     def __gt__(self, other):
-        pass
+        return self.a * other.b > self.b * other.a
 
     def __lt__(self, other):
-        pass
+        return self.a * other.b < self.b * other.a
 
     def __str__(self):
         return f"Fraction: {self.a}, {self.b}"
